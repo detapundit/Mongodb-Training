a9d438bc6b43dd1797af3e29b72ec1b9ffe62693
Detapundit <144154692+detapundit@users.noreply.github.com>
2024-09-30 14:56:06 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Backup path
+BACKUPS_DIR="/opt/moblize/backup"
+
+# Define the input file
+INFILE=/root/dblist.txt
+
+# Read the input file line by line
+while read -r LINE
+do
+    printf '%s\n' "$LINE" >> /coll_backup.log 
+  mongodump --db $LINE --collection collname --gzip --out $BACKUPS_DIR
+done < "$INFILE"
