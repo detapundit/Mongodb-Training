d9cbd9dfcd6e0cc48ba8af1c19a70b0676d48a86
Detapundit <144154692+detapundit@users.noreply.github.com>
2024-10-02 22:13:30 +0530

    Update sc2.sh

--- sc2.sh
+++ sc2.sh
@@ -2,11 +2,11 @@
 
 
 # Define the input file
-INFILE=/opt/moblize/dblist.txt
+INFILE=/opt/dblist.txt
 
 # Read the input file line by line
 while read -r LINE
 do
     printf '%s\n' "$LINE" >> /coll_size.log
-        mongo $LINE --host 172.31.1.44 --eval "db.sampleColl.totalSize()" >> /output.log
+        mongo $LINE --host 179.31.1.44 --eval "db.sampleColl.totalSize()" >> /output.log
 done < "$INFILE"
